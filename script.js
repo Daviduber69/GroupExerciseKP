0934a02c12090737efe4e1908cda9d9a5934f5a3
David Henriksson <davhen0108@skola.goteborg.se>
2024-03-28 10:17:31 +0100

    hej

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-fetch('https://icanhazdadjoke.com/',
-{headers: {'Accept': 'application/json', 'User-Agent': 'yrgo-kprog-class'}})
-.then(resp => resp.json())
-.then(json => console.log(json.joke));
 
-let jokeBox = document.getElementsByClassName("jokeBox");
+function fetchJoke(url, id) {
+    fetch(url, {
+        headers: {
+            'Accept': 'application/json',
+            'User-Agent': 'yrgo-kprog-class'
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Something went wrong: ", response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            let jokeDiv = document.getElementById(id);
+            if (url === "https://icanhazdadjoke.com/") {
+                jokeDiv.innerHTML = "<pre>"+data.joke;
+            }
+        })
+        .catch(error => {
+            console.error("Error: ", error);
+        })
+}
+
+
+
+let jokeBox = document.getElementById("jokes");
 let jokeButton = document.getElementById("jokeButton");
+let rating = document.getElementById("ratingForm");
 
-jokeButton.addEventListener("click", ()=>{
-    document.body.innerHTML.joke;
+jokeButton.addEventListener("click", () => {
+    fetchJoke("https://icanhazdadjoke.com/", "jokes");
 })
+
+rating.addEventListener("submit", ()=>{
+
+});
